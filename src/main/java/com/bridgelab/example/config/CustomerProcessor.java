8e5d7d64647105b3d3a9965221cd9baa3695cc24
Shraddha <jadhavshradddha5@gmail.com>
2023-10-10 11:27:40 +0530

    This is example of Spring batch

---
+++ src/main/java/com/bridgelab/example/config/CustomerProcessor.java
@@ -0,0 +1,18 @@
+package com.bridgelab.example.config;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.bridgelab.example.entity.Customer;
+
+public class CustomerProcessor implements ItemProcessor<Customer,Customer> {
+	
+	  @Override
+	    public Customer process(Customer customer) throws Exception {
+	        if(customer.getCountry().equals("United States")) {
+	            return customer;
+	        }else{
+	            return null;
+	        }
+	    }
+
+}
